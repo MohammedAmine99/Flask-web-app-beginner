9cd2bdff327474785a5916cfc8c029a50faf3db0
MohammedAmine99 <aminekh890@gmail.com>
2026-08-23 15:43:06 +0100

    add models

--- website/__init__.py
+++ website/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'your_secret_key'  # Replace with your own secret key
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # Import and register blueprints here
     from .views import views
@@ -10,4 +17,16 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Note
+
+    create_database(app)
+
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():
+            db.create_all()
+        print('Created Database !')
+
+        
--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,9 @@
-from flask import Blueprint, render_template            
+from flask import Blueprint, render_template , request, flash , redirect , url_for
+from . import db
+from .models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
 
 
 auth = Blueprint('auth', __name__)
@@ -7,14 +12,41 @@ auth = Blueprint('auth', __name__)
 def home():
     return render_template("home.html")
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html", boolean=True, text="This is the login page", user="John Doe")
 
 @auth.route('/logout')
 def logout():
     return render_template("home.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        passwordConfirm = request.form.get('passwordConfirm')
+
+        if len(email) < 4:
+            flash('Email must be greather than  4 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greather than 2 characters.', category='error')
+        elif len(lastName) < 2:
+            flash('Last name must be greather than 2 characters.', category='error')
+        elif password != passwordConfirm:
+            flash('Passwords do not match.', category='error')
+        elif len(password) < 7:
+            flash('Password must be at least 7 characters long.', category='error')
+        else:
+            new_user = User(email=email, first_name=firstName, last_name=lastName, password=generate_password_hash(password, method='pbkdf2:sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Account created successfully!', category='success')
+            return redirect(url_for('views.home'))
+            #add user to database
+
     return render_template("sign_up.html")
